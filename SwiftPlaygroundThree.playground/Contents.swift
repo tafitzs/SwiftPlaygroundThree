a8cd43f5666fb44117d906f373edab3b19cab25f
tafitzs <tafitzs@hawaii.edu>
2018-02-12 10:14:19 -1000

    Completed SwiftPlaygroundThree

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -3,3 +3,38 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//declare variable
+
+var fruitBasket: [String]
+
+//simple string variable
+
+var simpleString: String
+
+//initialize an array using an Array literal
+
+fruitBasket = ["Grapefruit", "Kiwi", "Apple", "Mango"]
+
+print(fruitBasket)
+
+//for-in loop
+
+for name in fruitBasket{
+   //print "I like to eat" before every name in fruitBasket
+    print("I like to eat " + name)
+}
+
+//lets print out item at index 1
+
+print(fruitBasket[1])
+
+//print out item at index 0
+
+print(fruitBasket[0])
+
+
+
+
+
+
